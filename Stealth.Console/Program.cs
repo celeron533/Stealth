e258bc7375c1d914aadfa37a235525810147315c
celeron533 <zhuyilun8@hotmail.com>
2015-09-24 22:56:02 +0800

    split project

---
+++ Stealth.Console/Program.cs
@@ -0,0 +1,23 @@
+﻿using Stealth.Core.WindowInstance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stealth.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WindowInstanceService windowInstanceService = new WindowInstanceService();
+            var list = windowInstanceService.GetWindowInstanceInfoDetailList()
+                .Where(c=>c.isWindowVisible && !string.IsNullOrEmpty(c.windowTitle));
+            foreach(var item in list)
+            {
+                System.Console.WriteLine(item.ToString());
+            }
+            System.Console.ReadLine();
+        }
+    }
+}
---
+++ Stealth.Core/WindowInstance/WindowInstanceInfoBase.cs
@@ -0,0 +1,41 @@
+﻿using Stealth.Core.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Stealth.Core.WindowInstance
+{
+    public class WindowInstanceInfoBase
+    {
+        public WindowInstanceInfoBase(IntPtr hWnd)
+        {
+            this.hWnd = hWnd;
+        }
+
+        private IntPtr _hWnd;
+        public IntPtr hWnd
+        {
+            private set { _hWnd = value; }
+            get { return _hWnd; }
+        }
+
+        public string windowTitle
+        {
+            get
+            {
+                StringBuilder strbTitle = new StringBuilder(255);
+                User32.GetWindowText(hWnd, strbTitle, strbTitle.Capacity + 1);
+                return strbTitle.ToString();
+            }
+        }
+
+        public bool isWindowVisible
+        {
+            get
+            {
+                return User32.IsWindowVisible(hWnd);
+            }
+        }
+    }
+}
