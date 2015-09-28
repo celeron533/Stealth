7c49b537ff22106771ec1592a8b176f96f25e685
celeron533 <zhuyilun8@hotmail.com>
2015-09-28 08:33:13 +0800

    Add arguments for testing. Using [CommandLineParser]

--- Stealth.Console/Program.cs
+++ Stealth.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using Stealth.Core.WindowInstance;
+﻿using Stealth.Core.Utilities;
+using Stealth.Core.WindowInstance;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,7 +11,44 @@ namespace Stealth.Console
     {
         static void Main(string[] args)
         {
-            while (true)
+            Options options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                if (options.hWnd==0)
+                {
+                    WindowInstanceService windowInstanceService = new WindowInstanceService();
+                    var list = windowInstanceService.GetWindowInstanceInfoDetailList()
+                        .Where(c => c.isWindowVisible && !string.IsNullOrEmpty(c.windowTitle));
+                    foreach (var item in list)
+                    {
+                        System.Console.WriteLine(item.ToString());
+                    }
+                }
+                else
+                {
+                    var window = new WindowInstanceInfoDetail((IntPtr)options.hWnd);
+                    if (options.isReset)
+                    {
+                        window.transparencyProperty.bAlpha = 255;
+                        window.transparencyProperty.dwFlags = (uint)User32.LWA.LWA_UNDEFINED;
+                    }
+                    else
+                    {
+                        window.isLayered = true;
+                        window.transparencyProperty.bAlpha = (byte)options.bAlpha;
+                        window.transparencyProperty.dwFlags = (uint)User32.LWA.LWA_ALPHA;
+                    }
+                }
+            }
+            else
+            {
+                // Display the default usage information
+                System.Console.WriteLine(options.GetUsage());
+            }
+
+
+            //////////////
+            while (false)
             {
                 System.Console.WriteLine("=====");
                 WindowInstanceService windowInstanceService = new WindowInstanceService();
@@ -18,8 +56,13 @@ namespace Stealth.Console
                     .Where(c => c.isWindowVisible && !string.IsNullOrEmpty(c.windowTitle));
                 foreach (var item in list)
                 {
+                    if (item.windowTitle== "无标题 - 记事本")
+                    {
+                        item.isLayered = !item.isLayered;
+                        item.transparencyProperty.dwFlags = (uint)User32.LWA.LWA_ALPHA;
+                        item.transparencyProperty.bAlpha = 200;
+                    }
                     System.Console.WriteLine(item.ToString());
-                    System.Console.WriteLine("{0}, {1}, {2}", item.transparencyProperty.crKey, item.transparencyProperty.bAlpha, item.transparencyProperty.dwFlags);
                 }
                 System.Console.ReadLine();
             }
